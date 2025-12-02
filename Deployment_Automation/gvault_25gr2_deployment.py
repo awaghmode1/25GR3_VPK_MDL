990cfd1e83883e097d1e55326533f6ec79047eea
awaghmode1 <Ashwini.Waghmode1@gilead.com>
2025-12-02 21:55:16 +0530

    Update gvault_25gr2_deployment.py

--- Deployment_Automation/gvault_25gr2_deployment.py
+++ Deployment_Automation/gvault_25gr2_deployment.py
@@ -391,7 +391,7 @@ mdl_headers = {'Authorization': sessionID,'Accept':'application/json','Content-T
 #Reading the input file
 ensure_csv_not_open(input_file)
 #df = pd.read_csv(input_file)
-df = pd.read_csv(input_file, skiprows=1)  # if header starts on line 2
+df = pd.read_csv(input_file, compression='infer')
 for idx, row in df.iterrows():
     start_time = datetime.now().strftime("%H:%M:%S")
     if row['File Type'] == 'VPK' and vpk_deployment_type is True and row['Step Status'] != 'Completed':
